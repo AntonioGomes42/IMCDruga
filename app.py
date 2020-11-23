a55d7bc1b5f278318b586e6cc1772249658831c1
Antonio Francisco Silva Gomes <73271272+AntonioGomes42@users.noreply.github.com>
2020-11-23 00:25:08 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import (Flask, render_template, request)
+
+app = Flask(__name__)
+
+def is_number_tryexcept(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def imc_img(resultado):
+    index = 'index.html'
+
+    if resultado < 18.5:
+        if resultado<1 :
+            frase_madruga = "\"Quando a fome aperta, a vergonha afrouxa...\""
+            resultado = "o peso abaixo do ideal! Seu imc é: " + str(resultado) + " ."
+            imagem = '/static/assets/magro_mais.gif'                  
+            return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+        else:
+            frase_madruga = "\"Quando a fome aperta, a vergonha afrouxa...\""
+            resultado = "o peso abaixo do ideal! Seu imc é: " + format(resultado, '.2f') + " ."
+            imagem = '/static/assets/magro_mais.gif'                  
+            return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+        
+    elif resultado>= 18.5 and resultado<=24.9: 
+        frase_madruga = "\"Sou pobre, porém honrado!\""
+        resultado = "o peso ideal! Seu imc é: " + format(resultado, '.2f') + " ."
+        imagem = '/static/assets/normal.gif'
+        return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+    
+    elif resultado > 24.9 :
+        frase_madruga = "\"Sou um homem de muita barriga senhor sorte.\""
+        resultado = "o peso acima do ideal! Seu imc é: " + format(resultado, '.2f') + " ."
+        imagem = '/static/assets/seu_barriga.gif'
+        return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+    
+    else:
+        resultado = "Por favor, insira dígitos válidos."
+        frase_madruga = "\"Eu sabia que você era idiota, mas não a nível executivo!\""
+        imagem = '/static/assets/invalido.gif'
+        return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+    
+
+@app.route('/', methods=['GET', 'POST'] )
+def index():
+    frase_madruga= None
+    imagem = None
+    n1 = None
+    n2 = None
+    resultado =  None
+    index = 'index.html'
+
+    if request.method == 'POST':
+        if is_number_tryexcept(request.form['altura_number'].replace(',','.',1)) and is_number_tryexcept(request.form['peso_number'].replace(',','.',1)): 
+            n1 = float(request.form['altura_number'].replace(',','.',1))
+            n2 = float(request.form['peso_number'].replace(',','.',1))
+
+            if n1>0 and n2>0:  
+                resultado = n2/(n1*n1)
+                n = imc_img(resultado)          
+            return n
+        else:
+            resultado = "Por favor, insira dígitos válidos."
+            frase_madruga = "\"Eu sabia que você era idiota, mas não a nível executivo!\""
+            imagem = '/static/assets/invalido.gif'
+            return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)     
+
+    return render_template( index, frase_madruga = frase_madruga, resultado = resultado, imagem = imagem)
+
+if __name__ == "__main__":
+    app.run(debug=True)
